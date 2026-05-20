b934960b4eb586b024bfc2cc06b84790611dcdaf
2502031830077-ctrl <2502031830077@silveroakuni.ac.in>
2026-05-20 13:56:24 +0530

    Initial commit

---
+++ my-app/src/App.jsx
@@ -0,0 +1,18 @@
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+// import './App.css'
+
+import Hello from './Hello'
+
+function App() {
+
+  return (
+    <>
+      <h1>Welcome to My App</h1>
+      <Hello />
+    </>
+  )
+}
+
+export default App
